35b4dee72b7503d1cfc9b3fd10a438b106da3970
Tivalsdeveloper <tivalsdeveloper@gmail.com>
2026-08-19 11:16:48 +0200

    Add TivelText example

---
+++ examples/basic.py
@@ -0,0 +1,6 @@
+from tiveltext import text, decorate
+
+print(text("TIVALS", font="block", color="cyan"))
+print(text("TivelText", font="gothic", color="#ff66ff"))
+print(text("Developer", font="script"))
+print(decorate("TivelText 1.0.0", "double"))
